ab7c1f6895ca0328b3a7e64537e6f744c796899a
Mahmoud-alim <mahmoudhusseinalim@gmail.com>
2021-03-07 23:25:05 +0200

    observe on the Livedata through ViewModel with SwitchMap (passing the repository)

--- app/src/main/java/com/mahmoudalim/picsapp/ui/MainActivity.kt
+++ app/src/main/java/com/mahmoudalim/picsapp/ui/MainActivity.kt
@@ -2,11 +2,13 @@ package com.mahmoudalim.picsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
 import com.mahmoudalim.picsapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/mahmoudalim/picsapp/ui/fragments/GalleryFragment.kt
+++ app/src/main/java/com/mahmoudalim/picsapp/ui/fragments/GalleryFragment.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import com.mahmoudalim.picsapp.R
+import com.mahmoudalim.picsapp.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlin.properties.Delegates
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
+    private lateinit var binding: FragmentGalleryBinding
 
+    private val viewModel by viewModels<GalleryViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentGalleryBinding.bind(view)
+
+        viewModel.photos.observe(viewLifecycleOwner) {
+        }
+    }
 }
